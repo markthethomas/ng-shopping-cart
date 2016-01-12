95e8d14af80bb1411878d850be75568ba5dfdc86
Mark Thomas <markthethomas@gmail.com>
2016-01-12 14:21:24 -0800

    Clean things and use generator-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngShoppingCartApp
+ * @description
+ * # ngShoppingCartApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngShoppingCartApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
